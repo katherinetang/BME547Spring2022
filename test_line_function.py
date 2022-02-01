66cf0ed633a41ceb550bac7fb5cccf54af3aad38
Katherine <katherinedtang@gmail.com>
2022-02-01 14:10:17 -0500

    Altered the tests performed to be slightly more diverse

--- test_line_function.py
+++ test_line_function.py
@@ -1,9 +1,9 @@
 import pytest
 
 @pytest.mark.parametrize("point1, point2, x, expected", [
-    [(1, 1), (4, 4), 8, 8],
+    [(1, 1), (4, 7), 8, 15],
     [(2, 4), (6, 12), 3, 6],
-    [(-3, -1), (-15, -5), -18, -6]
+    [(-3, -1), (-13, -5), 5, 11/5]
     ])
 def test_find_y(point1, point2, x, expected):
     from line_function import find_y
